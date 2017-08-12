6faab594bf5511fb31e5a41e5e063559650691e2
Paco <Paco@192.168.1.36>
2017-08-12 17:44:39 +0200

    Initial commit.

---
+++ spring-boot-example/src/test/java/es/pruebas/SpringBootExampleApplicationTests.java
@@ -0,0 +1,18 @@
+package es.pruebas;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.Assert;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		Assert.isTrue(true);
+	}
+
+}
